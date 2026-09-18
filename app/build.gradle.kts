a0a47d4d1baacc877e1ee22c691ef0fe925003ab
liderkizilkaya-sys <liderkizilkaya@gmail.com>
2026-09-18 05:04:58 +0300

    Bump Android release to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.golgeprotokolu.game"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     val ksPath = System.getenv("GP_KEYSTORE_PATH")
