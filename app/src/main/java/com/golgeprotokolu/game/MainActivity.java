bb646b5924b8242ea6286ab900dcc155bb9f245e
liderkizilkaya-sys <liderkizilkaya@gmail.com>
2026-09-18 05:04:55 +0300

    Enable persistent local game progress

--- app/src/main/java/com/golgeprotokolu/game/MainActivity.java
+++ app/src/main/java/com/golgeprotokolu/game/MainActivity.java
@@ -9,21 +9,34 @@ import android.webkit.WebViewClient;
 public class MainActivity extends Activity {
     private WebView webView;
 
-    @Override public void onCreate(Bundle state) {
+    @Override
+    public void onCreate(Bundle state) {
         super.onCreate(state);
+
         webView = new WebView(this);
-        WebSettings s = webView.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(false);
-        s.setAllowFileAccess(true);
-        s.setAllowContentAccess(false);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(false);
+        settings.setDatabaseEnabled(false);
+        settings.setSupportZoom(false);
+        settings.setBuiltInZoomControls(false);
+
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
     }
 
-    @Override protected void onDestroy() {
-        if (webView != null) webView.destroy();
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.loadUrl("about:blank");
+            webView.clearHistory();
+            webView.removeAllViews();
+            webView.destroy();
+            webView = null;
+        }
         super.onDestroy();
     }
 }
