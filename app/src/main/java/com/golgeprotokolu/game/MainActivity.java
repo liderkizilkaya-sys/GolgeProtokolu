9e75eb637a9153d46c75bd5b3dceaf47110fb34b
liderkizilkaya-sys <liderkizilkaya@gmail.com>
2026-09-18 03:04:10 +0300

    Add app/src/main/java/com/golgeprotokolu/game/MainActivity.java

---
+++ app/src/main/java/com/golgeprotokolu/game/MainActivity.java
@@ -0,0 +1,34 @@
+package com.golgeprotokolu.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        webView = new WebView(this);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(false);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+
+    @Override public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+
+    @Override protected void onDestroy() {
+        if (webView != null) webView.destroy();
+        super.onDestroy();
+    }
+}
